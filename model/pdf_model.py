57d677173f30ab89041e3d32fba319d2b01a171c
Felipe <anfellr11@mail.com>
2023-08-04 18:25:30 -0500

    Code refactory

--- model/pdf_model.py
+++ model/pdf_model.py
@@ -1,31 +1,16 @@
-import os
-import re
-import pandas as pd
-from pdfminer.high_level import extract_text
-from pdfminer.layout import LAParams
+import aspose.pdf as ap
 
 class PDFModel:
     def convert_pdf_to_excel(self, pdf_file, excel_file):
         try:
-            # Extraer texto del PDF utilizando pdfminer
-            text = extract_text(pdf_file, laparams=LAParams())
+            # Crear un objeto de tipo Document
+            doc = ap.Document(pdf_file)
 
-            # Convertir el texto en una lista de líneas
-            lines = text.split('\n')
+            # Crear un objeto de tipo ExcelSaveOptions
+            excel_save_options = ap.ExcelSaveOptions()
 
-            # Filtrar las líneas para eliminar líneas vacías y espacios en blanco
-            lines = [line.strip() for line in lines if line.strip()]
-
-            # Separar los datos de las facturas
-            # Aquí necesitarás implementar una lógica específica para tu caso, ya que cada factura puede tener un formato diferente
-            # En este ejemplo, simplemente se dividen las líneas en bloques de 5 líneas cada uno
-            data = [lines[i:i+5] for i in range(0, len(lines), 5)]
-
-            # Crear un DataFrame de pandas con los datos extraídos
-            df = pd.DataFrame(data, columns=['Columna 1', 'Columna 2', 'Columna 3', 'Columna 4', 'Columna 5'])
-
-            # Guardar el DataFrame en un archivo Excel
-            df.to_excel(excel_file, index=False)
+            # Guardar el archivo PDF como Excel
+            doc.save(excel_file, excel_save_options)
 
             return True, "Archivo creado exitosamente"
         except Exception as e:
