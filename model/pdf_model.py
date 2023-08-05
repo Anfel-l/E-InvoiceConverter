b9a88a01b381755ff6c542669927b3d2a44434cc
Felipe <anfellr11@mail.com>
2023-08-05 01:15:47 -0500

    Bug fixes

--- model/pdf_model.py
+++ model/pdf_model.py
@@ -1,30 +1,74 @@
 import os
+import pandas as pd
+import threading
 from aspose.pdf import Document, ExcelSaveOptions
 
 class PDFModel:
-    def convert_pdfs_to_excel(self, input_dir, excel_file):
+    def convert_pdfs_to_excel(self, input_dir, output_dir):
         try:
             # Obtener la lista de archivos PDF en el directorio de entrada
             pdf_files = [file for file in os.listdir(input_dir) if file.lower().endswith(".pdf")]
 
-            # Crear un objeto de tipo ExcelWorkbook
-            workbook = Document()
+            # Crear la carpeta para los archivos temporales si no existe
+            os.makedirs(output_dir, exist_ok=True)
 
-            for pdf_file in pdf_files:
-                pdf_path = os.path.join(input_dir, pdf_file)
+            # Dividir los archivos PDF en grupos de 4
+            pdf_groups = [pdf_files[i:i + 4] for i in range(0, len(pdf_files), 4)]
 
-                # Crear un objeto de tipo Document para el archivo PDF actual
-                doc = Document(pdf_path)
+            # Función para procesar un grupo de archivos PDF y guardar el resultado en un archivo de Excel temporal
+            def process_group(group):
+                workbook = Document()
 
-                # Agregar páginas del PDF actual al archivo de Excel
-                for page in doc.pages:
-                    workbook.pages.add(page)
+                for pdf_file in group:
+                    pdf_path = os.path.join(input_dir, pdf_file)
 
-            # Guardar el archivo de Excel con todas las páginas de los PDF
-            excelSaveOptions = ExcelSaveOptions()
-            workbook.save(excel_file, excelSaveOptions)
+                    # Crear un objeto de tipo Document para el archivo PDF actual
+                    doc = Document(pdf_path)
 
-            return True, "Archivos creados exitosamente"
+                    # Agregar páginas del PDF actual al archivo de Excel
+                    for page in doc.pages:
+                        workbook.pages.add(page)
+
+                # Guardar el archivo de Excel temporal con todas las páginas de los PDF
+                excel_file = os.path.join(output_dir, f"temp_{group[0]}.xlsx")
+                excelSaveOptions = ExcelSaveOptions()
+                workbook.save(excel_file, excelSaveOptions)
+
+            # Crear y ejecutar un hilo para cada grupo de archivos PDF
+            threads = []
+            for group in pdf_groups:
+                t = threading.Thread(target=process_group, args=(group,))
+                threads.append(t)
+                t.start()
+
+            # Esperar a que todos los hilos terminen
+            for t in threads:
+                t.join()
+
+            return True, "Archivos temporales creados exitosamente"
         except Exception as e:
             print(e)
-            return False, f"Error al crear los archivos: {str(e)}"
+            return False, f"Error al crear los archivos temporales: {str(e)}"
+
+    def combine_temporales_with_pandas(self, temp_dir, excel_file):
+        try:
+            # Lista para almacenar DataFrames temporales
+            dfs = []
+
+            # Leer cada archivo temporal y almacenar su contenido en la lista de DataFrames
+            for file in os.listdir(temp_dir):
+                if file.lower().endswith(".xlsx"):
+                    excel_file_path = os.path.join(temp_dir, file)
+                    df = pd.read_excel(excel_file_path)
+                    dfs.append(df)
+
+            # Combinar todos los DataFrames en uno solo
+            all_data = pd.concat(dfs, ignore_index=True)
+
+            # Guardar el archivo de Excel final
+            all_data.to_excel(excel_file, index=False)
+
+            return True, "Archivos temporales combinados y creados exitosamente"
+        except Exception as e:
+            print(e)
+            return False, f"Error al combinar los archivos temporales: {str(e)}"
