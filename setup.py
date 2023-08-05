70f0965e320b638867013c6cce5e1cefeb992d63
Felipe <anfellr11@mail.com>
2023-08-05 01:56:58 -0500

    Installation script created, also threading process for pdf conversion

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ executables = [Executable("main.py", base=base)]
 setup(
     name="Business Laboratory Conversion App",
     version="1.0",
-    description="Descripción de tu aplicación",
+    description="The E-Invoice Converter App is an application that allows you to convert PDF and XML files to Excel format with ease. This tool is useful for processing and analyzing data contained in PDF and XML files and working with it in an Excel spreadsheet.",
     options={"build_exe": {"packages": ["os"], "excludes": []}},
     executables=executables
 )
