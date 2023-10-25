247342695bdf779c2b9c5582bcc08634395c3a06
Anfel-l <anfellr11@gmail.com>
2023-10-25 09:18:50 -0500

    Merging commit test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt6.QtWidgets import QApplication
 from controller.controller import Controller
 from view.views import MainWindow
 
+'''Main method for execution'''
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     controller = Controller()
