f9378959a6980e56a80ac05cdfdc407bc4740be6
Anfel-l <anfellr11@gmail.com>
2024-02-01 18:41:04 -0500

    .bat file tested, .sh file tested

--- install.bat
+++ install.bat
@@ -1,4 +1,8 @@
 @echo off
+python -m pip install  PyQt6
+python -m pip install  pandas
+python -m pip install openpyxl
+python -m pip install pyinstaller
 python -m pip install pyinstaller
 pyinstaller --onefile --windowed --name "E-Invoice Converter" main.py
 pause
--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+python.exe -m pip install  PyQt6
+python.exe -m pip install  pandas
+python.exe -m pip install openpyxl
 python.exe -m pip install pyinstaller
 pyinstaller --onefile --windowed --name 'E-Invoice Converter' main.py
