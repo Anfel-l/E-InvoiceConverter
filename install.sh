f103a2b697abaa304036c8192c2c3a5ed350df8b
Anfeldev <anfellr11@gmail.com>
2025-11-14 21:29:55 -0500

    update: updated install file

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-python.exe -m pip install  PyQt6
-python.exe -m pip install  pandas
-python.exe -m pip install openpyxl
-python.exe -m pip install pyinstaller
+
+if [ ! -d "venv" ]; then
+    echo "Creating env"
+    python3 -m venv venv
+
+fi
+
+source venv/bin/activate
+
+echo "Installing dependencies"
+python3 -m pip install --upgrade pip
+python3 -m pip install PyQt6
+python3 -m pip install pandas
+python3 -m pip install openpyxl
+python3 -m pip install pyinstaller
+
+echo "Creating executable file"
 pyinstaller --onefile --windowed --name 'E-Invoice Converter' main.py
+echo "Installation complete!"
